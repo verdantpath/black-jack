fd5f161e345fa97607dc88a829c69730efde8f8c
verdantpath <m_dittmann@yahoo.com>
2018-09-27 17:37:03 -0700

    update js

--- scripts/my_scripts.js
+++ scripts/my_scripts.js
@@ -62,7 +62,10 @@ $(function() {
   ]
 });
 
+// create an array to hold the used cards
 var used_cards = new Array();
+
+// start out by dealing 2 cards
 function deal() {
   for(var i = 0; i < 2; i++;) {
     hit();
@@ -72,3 +75,23 @@ function getRandom(num) {
   var my_num = Math.floor(Math.random() * num);
   return my_num;
 }
+function hit() {
+  var good_card = false;
+  do {
+    var index = getRandom(52);
+    if ( !$.inArray(index, used_cards) > -1 ) {
+      good_card = true;
+      var c = deck[index];
+      used_cards[used_cards.length] = index;
+      hand.cards[hand.cards.length] = c;
+      var $d = $("<div>");
+      $d.addClass("current_hand").appendTo("#my_hand");
+      $('<img>').appendTo($d).attr("src", 'images/cards/' + c.suit + '/' + c.name + '.jpg').fadeOut('slow').fadeIn('slow');
+    }
+  } while(!good_card);
+  good_card = false;
+}
+$("#btnDeal").click( ) {
+  deal();
+  $(this).toggle();
+});
